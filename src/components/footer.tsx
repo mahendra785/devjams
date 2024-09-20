6d9e5115c7c15ee01475ca652c314d24393bf674
Van5sh <vansh05dhir@gmail.com>
2024-09-20 17:36:23 +0530

    fix:navbar and footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,12 +7,12 @@ const Footer = () => {
     const iconVariants = {
         whileHover: { rotate: 360, scale: 1.2 },
         whileTap: { rotate: -360, scale: 0.8 },
-        whileFocus: { rotate: 360, scale: 1.2 },
+        whileFocus: { rotate: 360, scale: 1.2 },    
     };
 
     return (
-        <footer className="bg-gray-800 text-white py-6">
-            <motion.div className="max-w-screen-xl mx-auto px-6 flex justify-end items-center space-x-8">
+        <footer className="bg-transparent text-white py-6 w-[100vw]">
+            <motion.div className="max-w-screen-xl mx-auto px-3 flex justify-center items-center space-x-8">
                 <motion.div variants={iconVariants} whileHover="whileHover" whileTap="whileTap">
                     <FaFacebook size={30} />
                 </motion.div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-transparent md:py-5">
-      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8 flex justify-end items-center">
-        <ul className="flex gap-2 sm:gap-4 md:gap-6 list-none">
+    <nav className="bg-transparent md:py-5 w-full">
+      <div className="max-w-screen-xl mx-auto px-4">
+        <ul className="flex justify-end gap-2 sm:gap-4 md:gap-6 list-none w-full">
           <li>
             <a
               href="/"
-              className={`block py-2 px-2 md:px-3 text-white-500 font-bold hover:text-grey-700 text-sm md:text-base`}
+              className={`block py-2 px-2 md:px-3 text-white font-bold hover:text-gray-700 text-sm md:text-base`}
             >
               HOME
             </a>
@@ -16,7 +16,7 @@ const Navbar: React.FC = () => {
           <li>
             <a
               href="/"
-              className={`block py-2 px-2 md:px-3 text-white-500 font-bold hover:text-green-700 text-sm md:text-base`}
+              className={`block py-2 px-2 md:px-3 text-white font-bold hover:text-gray-700 text-sm md:text-base`}
             >
               SIGN-IN
             </a>
@@ -24,7 +24,7 @@ const Navbar: React.FC = () => {
           <li>
             <a
               href="/"
-              className={`block py-2 px-2 md:px-3 text-white-500 font-bold hover:text-green-700 text-sm md:text-base`}
+              className={`block py-2 px-2 md:px-3 text-white font-bold hover:text-gray-700 text-sm md:text-base`}
             >
               TEAM
             </a>
